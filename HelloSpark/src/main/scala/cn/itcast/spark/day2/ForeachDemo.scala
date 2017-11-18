8c986c65891a33a206965965287f3dcfa6fcbc87
ly923976094 <923976094@qq.com>
2017-11-18 15:04:32 +0800

    hello spark foreachparation

--- HelloSpark/src/main/scala/cn/itcast/spark/day2/ForeachDemo.scala
+++ HelloSpark/src/main/scala/cn/itcast/spark/day2/ForeachDemo.scala
@@ -10,6 +10,8 @@ object ForeachDemo {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("ForeachDemo").setMaster("local")
     val sc = new SparkContext(conf)
+    val rdd1 = sc.parallelize(List(1,2,3,4,5,6,7,8,9),3)
+//    rdd1.foreachPartition()
     //
     //
 
